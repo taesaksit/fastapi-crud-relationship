15d92192be5735ce4ccf4d07cec5b0ac78e575ca
taepc <taesaksit09@gmail.com>
2025-07-13 22:18:53 +0700

    update schema

--- schemas/book.py
+++ schemas/book.py
@@ -5,13 +5,13 @@ from .category import CategoryResponse
 class BookBase(BaseModel):
     title: str
     author: Optional[str]
-    category_id: int
     
 class BookCreate(BookBase):
     title: str = Field(strip_whitespace=True, min_length=3)
-
+    category_id: int
+    
 class BookResponse(BookBase):
     id: int
-    category: Optional[CategoryResponse]
+    category:CategoryResponse
     class Config:
         from_attributes = True
